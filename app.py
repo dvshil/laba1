3d0a850c672baf6caafa83b54a0b350bd5b173f4
dvshil <sadhbmm@gmail.com>
2026-09-18 14:53:12 +0300

    second modify divide function in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ print('one more')
 def add(a, b): return a + b
 def subtract(a, b): return a - b
 def multiply(a, b): return a * b
-def divide(a, b): return a / b
+def divide(a, b): return a // b
 def power(a, b): return a ** b
 def modulo(a, b): return a % b
 def union(a, b): return str(a) + str(b)
