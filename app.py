9cfa66d0b63344d75dbdf72caf4998a97962148a
dvshil <sadhbmm@gmail.com>
2026-09-18 14:36:05 +0300

    fix syntax error in app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ print('hello')
 print('modified')
 print('one more')
 
-def add(a, b): retuurn a + b
+def add(a, b): return a + b
 def subtract(a, b): return a - b
 def multiply(a, b): return a * b
 def divide(a, b): return a / b
