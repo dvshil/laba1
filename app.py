8db201e1cdaeea6ba99d2e848e5bec2b62d3740e
dvshil <sadhbmm@gmail.com>
2026-09-19 12:28:08 +0300

    address review comments^ fix average function. revert unrelated divide change

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ print('one more')
 def add(a, b): return a + b
 def subtract(a, b): return a - b
 def multiply(a, b): return a * b
-def divide(a, b): return a / b if b != 0 else None
+def divide(a, b): return a // b
 def power(a, b): return a ** b
 def modulo(a, b): return a % b
 def union(a, b): return str(a) + str(b)
-def averange(a, b): return (a + b) // 2
+def averange(a, b): return (a + b) / 2
